5ae786c64e9996441a295556b314a863ac8d80c4
paipo.tang <paipo.tang@rakuten.com>
2018-05-12 19:34:50 +0900

    Add scrolling effect

---
+++ main.js
@@ -0,0 +1,18 @@
+;(function(window, document) {
+  // Add scrolling animation
+  document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault()
+      removeCurrentSelectedClass()
+      this.classList.add('selected')
+      var $href = this.getAttribute('href')
+      document.querySelector($href).scrollIntoView({behavior: 'smooth'})
+    })
+  })
+
+  function removeCurrentSelectedClass() {
+    document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+      anchor.classList.remove('selected')
+    })
+  }
+})(window, document)
