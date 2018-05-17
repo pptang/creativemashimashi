4c9b8cb772eb3b1eb95af186d8bf5972c7bf39a5
pptang <donaldduck518@gmail.com>
2018-05-17 09:50:18 +0900

    Add logo and team member photos

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-;(function(window, document) {
+function scrollToContactSection() {
+  document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
+}
+
+(function(window, document) {
   // Add scrolling animation
   document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
     anchor.addEventListener('click', function(e) {
-      e.preventDefault()
-      removeCurrentSelectedClass()
-      this.classList.add('selected')
-      var $href = this.getAttribute('href')
-      document.querySelector($href).scrollIntoView({behavior: 'smooth'})
-    })
-  })
+      e.preventDefault();
+      removeCurrentSelectedClass();
+      this.classList.add('selected');
+      var $href = this.getAttribute('href');
+      document.querySelector($href).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
 
   function removeCurrentSelectedClass() {
     document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
-      anchor.classList.remove('selected')
-    })
+      anchor.classList.remove('selected');
+    });
   }
-})(window, document)
+})(window, document);
