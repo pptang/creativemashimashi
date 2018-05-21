e7c8e35f3e4f900ac048b2747698d5808beea250
paipo.tang <paipo.tang@rakuten.com>
2018-05-21 14:18:53 +0900

    Add yeoman generator

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,16 @@
 function scrollToContactSection() {
   // document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
   var scroll = new SmoothScroll()
-  scroll.animateScroll(document.querySelector("#contact"))
+  scroll.animateScroll(document.querySelector('#contact'))
 }
 
 ;(function(window, document) {
   // Add scrolling animation
   document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
-    anchor.addEventListener("click", function(e) {
+    anchor.addEventListener('click', function(e) {
       e.preventDefault()
       removeCurrentSelectedClass()
-      this.classList.add("selected")
+      this.classList.add('selected')
       // var $href = this.getAttribute('href');
       // document.querySelector($href).scrollIntoView({ behavior: 'smooth' });
     })
@@ -20,17 +20,17 @@ function scrollToContactSection() {
 
   function removeCurrentSelectedClass() {
     document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
-      anchor.classList.remove("selected")
+      anchor.classList.remove('selected')
     })
   }
 
-  document.addEventListener("scroll", function() {
+  document.addEventListener('scroll', function() {
     var $document = document.documentElement
-    var $header = document.querySelector(".header")
+    var $header = document.querySelector('.header')
     if ($document.scrollTop > 105) {
-      $header.classList.add("blue-background")
+      $header.classList.add('blue-background')
     } else {
-      $header.classList.remove("blue-background")
+      $header.classList.remove('blue-background')
     }
   })
 })(window, document)
