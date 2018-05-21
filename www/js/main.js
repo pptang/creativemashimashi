90b43e385800b6f88a3a240d5268c8c7f76ae269
paipo.tang <paipo.tang@rakuten.com>
2018-05-21 09:18:05 +0900

    Add navbar color change animation

--- www/js/main.js
+++ www/js/main.js
@@ -1,26 +1,36 @@
 function scrollToContactSection() {
   // document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
-  var scroll = new SmoothScroll();
-  scroll.animateScroll(document.querySelector('#contact'));
+  var scroll = new SmoothScroll()
+  scroll.animateScroll(document.querySelector("#contact"))
 }
 
-(function(window, document) {
+;(function(window, document) {
   // Add scrolling animation
   document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      removeCurrentSelectedClass();
-      this.classList.add('selected');
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault()
+      removeCurrentSelectedClass()
+      this.classList.add("selected")
       // var $href = this.getAttribute('href');
       // document.querySelector($href).scrollIntoView({ behavior: 'smooth' });
-    });
-  });
+    })
+  })
 
-  var scroll = new SmoothScroll('a[href*="#"]', { offset: 150 });
+  var scroll = new SmoothScroll('a[href*="#"]', {offset: 150})
 
   function removeCurrentSelectedClass() {
     document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
-      anchor.classList.remove('selected');
-    });
+      anchor.classList.remove("selected")
+    })
   }
-})(window, document);
+
+  document.addEventListener("scroll", function() {
+    var $document = document.documentElement
+    var $header = document.querySelector(".header")
+    if ($document.scrollTop > 105) {
+      $header.classList.add("blue-background")
+    } else {
+      $header.classList.remove("blue-background")
+    }
+  })
+})(window, document)
